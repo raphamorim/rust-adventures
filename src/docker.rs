c2f50236a9509c5d9f292230ada793c2ab9c13cb
Raphael Amorim <rapha850@gmail.com>
2016-10-17 17:57:44 -0200

    adding gitignore

--- src/docker.rs
+++ src/docker.rs
@@ -1,16 +1,16 @@
 #![crate_type = "lib"]
-#![crate_name = "docker"]
+#![crate_name = "4"]
 
 pub fn public_function() {
-    println!("called rary's `public_function()`");
+    println!("called 4's `public_function()`");
 }
 
 fn private_function() {
-    println!("called rary's `private_function()`");
+    println!("called 4's `private_function()`");
 }
 
 pub fn indirect_access() {
-    print!("called rary's `indirect_access()`, that\n> ");
+    print!("called 4's `indirect_access()`, that\n> ");
 
     private_function();
 }
