554bd0e5724f2d9e8057d4ade17d0459011a1eb9
Raphael Amorim <rapha850@gmail.com>
2016-10-17 17:20:50 -0200

    creating functions

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,17 @@ fn main() {
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess);
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
 
-     println!("You guessed: {}", guess);
+    println!("You guessed: {}", guess);
+
+    let guess: u32 = guess.trim().parse()
+        .expect("Failed to parse");
+
+    scoring(guess);
 }
 
+fn scoring(n: u32) -> u32 {
+    n * 2
+}
